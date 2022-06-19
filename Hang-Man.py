ad376858b5058566265d8fd722394d529aad06af
Alex Campbell <alexcampbell@alexs-mac-mini.home>
2022-06-19 19:40:27 +0100

    added get_word

--- Hang-Man.py
+++ Hang-Man.py
@@ -1,12 +1,34 @@
 #!/usr/bin/python
-
-
+import random
+import csv
 def draw_hangman():
     hm = f" ________\n |/.    |\n |.     O\n |.    /|\\\n |.     |\n |.    / \\\n |.   |.  |\n |.   d.  b\n |__________ "
     print(hm)
 
 draw_hangman()
+
 def get_word(f):
     with open(f) as file:
-        data = file.read()
-        print(data)
+        row = csv.reader(file, delimiter=',')
+        words = list()
+        for word in row:
+            #print(word[0])
+            word = word[0].rstrip()
+            words.append(word)
+    i = random.randint(0,len(words)-1)
+    letters = list()
+    for l in words[i]:
+        letters.append(l)
+    return(letters)
+
+def create_guess(letters):
+    g = list()
+    for l in letters:
+        g.append("_")
+    return(g)
+
+ws = get_word(f="words.txt")
+guess = create_guess(ws)
+
+print(guess)
+print("mpty string")
