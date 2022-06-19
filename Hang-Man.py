b74c0f0b645ecdd842c4de8fd42c2cc9420d9c90
James Campbell <git@github.com>
2022-06-19 13:59:52 +0100

    Added example gallows

--- Hang-Man.py
+++ Hang-Man.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python
+
+
+def draw_hangman():
+    hm = f" ________\n |/.    |\n |.     O\n |.    /|\\\n |.     |\n |.    / \\\n |.   |.  |\n |.   d.  b\n |__________ "
+    print(hm)
+
+draw_hangman()
+
+
+
+
