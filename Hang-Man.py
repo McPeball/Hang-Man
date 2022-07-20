adc553d6a36c0b626ad1c1e4ffbd9f44e68888d3
James Campbell <git@github.com>
2022-07-20 23:37:26 +0100

    added all hangman ascii art sets and tidied up

--- Hang-Man.py
+++ Hang-Man.py
@@ -1,27 +1,56 @@
 #!/usr/bin/python
 import random
 import csv
-def draw_hangman():
-    hm = f" ________\n |/.    |\n |.     O\n |.    /|\\\n |.     |\n |.    / \\\n |.   |.  |\n |.   d.  b\n |__________ "
-    print(hm)
 
-draw_hangman()
+def draw_hangman(idx):
+    idx = int(idx)
+    #hm = f" ________\n |/     |\n |      O\n |     /|\\\n |      |\n |     / \\\n |    |   |\n |    d   b\n |__________ "
+    
+    hm = list([
+        f" \n\n\n\n\n\n\n\n  __________ ",
+        f" \n |       \n |       \n |        \n |       \n |        \n |         \n |         \n |__________ ",
+        f" ________\n |       \n |       \n |        \n |       \n |        \n |         \n |         \n |__________ ",
+        f" ________\n |/      \n |       \n |        \n |       \n |        \n |         \n |         \n |__________ ",
+        f" ________\n |/     |\n |       \n |        \n |       \n |        \n |         \n |         \n |__________ ",
+        f" ________\n |/     |\n |      O\n |        \n |       \n |        \n |         \n |         \n |__________ ",
+        f" ________\n |/     |\n |      O\n |      | \n |      |\n |        \n |         \n |         \n |__________ ",
+        f" ________\n |/     |\n |      O\n |     /| \n |      |\n |        \n |         \n |         \n |__________ ",
+        f" ________\n |/     |\n |      O\n |     /|\\\n |      |\n |        \n |         \n |         \n |__________ ",
+        f" ________\n |/     |\n |      O\n |     /|\\\n |      |\n |     /  \n |    |    \n |    d    \n |__________ ",
+        f" ________\n |/     |\n |      O\n |     /|\\\n |      |\n |     / \\\n |    |   |\n |    d   b\n |__________ "
+        ])
+    print(hm[idx])
+
+
+
+
 
 def get_word(f):
+    '''
+    Read words from a file (f). Randomly select one return a list
+    containing each letter from the word as an element in the list
+    '''
     with open(f) as file:
+        # misusing ces.reader() here
         row = csv.reader(file, delimiter=',')
         words = list()
         for word in row:
-            #print(word[0])
             word = word[0].rstrip()
             words.append(word)
+    # use i to pick a word randomly
     i = random.randint(0,len(words)-1)
+    # letters will hold each letter in the word as a list
     letters = list()
     for l in words[i]:
         letters.append(l)
     return(letters)
 
-def create_guess(letters):
+def create_guess_status(letters):
+    '''
+    Return a list, the same length as the list returned by get_word.
+    The list contains underscore characters that can be filled with
+    correctly guessed letters
+    '''
     g = list()
     for l in letters:
         g.append("_")
@@ -33,20 +62,35 @@ def check_guess(word, guess, letter):
             guess[i] = letter
     return(guess)
 
+def get_guess():
+    inp = input("please type a letter:\n")
+    inp.rstrip()
+    return(inp)
+    
 
+# Test hangman drawing
+draw_hangman(0)
+draw_hangman(1)
+draw_hangman(2)
+draw_hangman(3)
+draw_hangman(4)
+draw_hangman(5)
+draw_hangman(6)
+draw_hangman(7)
+draw_hangman(8)
+draw_hangman(9)
+draw_hangman(10)
 
 
 word = get_word(f="words.txt")
 
-#print(word)
+guessed = create_guess_status(word)
 
-guess = create_guess(word)
+guess = get_guess()
 
-inp = input("please type a letter\n")
-inp.rstrip()
-print(inp)
+guessed = check_guess(word, guessed, guess)
 
-guess = check_guess(word, guess, inp)
 
-print(guess)
-print("mpty string")
+print("".join(guessed))
+
+
